43f1186b40bd24f008cfa85b4f143da1fbad8468
Gnanod <1995gnanod@gmail.com>
2019-07-04 23:34:15 +0530

    no message

--- VehicleServiceCenter/BackEnd/src/main/java/lk/vsc/Repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package lk.vsc.Repository;
-
-import lk.vsc.Entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-}
---
+++ VehicleServiceCenter/BackEnd1/src/main/java/lk/vsc/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package lk.vsc.repository;
+
+
+import lk.vsc.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
