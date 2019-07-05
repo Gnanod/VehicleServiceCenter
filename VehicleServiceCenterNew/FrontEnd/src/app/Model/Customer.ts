acea0297aca2916705a7e1022cfab23861318e5a
Gnanod <1995gnanod@gmail.com>
2019-07-05 18:07:45 +0530

    no message

---
+++ VehicleServiceCenterNew/FrontEnd/src/app/Model/Customer.ts
@@ -0,0 +1,15 @@
+import {Vehicle} from "./Vehicle";
+
+export class Customer {
+    
+    customerId: Number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    nic: string;
+    address: string;
+    birthday: string;
+    phoneNumber: string;
+    vehicleSet :Array<Vehicle>;
+    
+}
