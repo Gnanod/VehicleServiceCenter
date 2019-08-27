590c3272823b2093d5f64cf8e721a20381bd4f7b
gnanod <1995gnanod@gmail.com>
2019-08-27 20:14:05 +0530

    no message

--- VehicleServiceCenterNew/BackEnd/src/main/java/lk/vsc/repository/StockRepository.java
+++ VehicleServiceCenterNew/BackEnd/src/main/java/lk/vsc/repository/StockRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface StockRepository extends JpaRepository<Stock,Integer> {
 
-    @Query(value = "select i.itemName,mm.makeName,mm.modelName,i.quantityOnHand from Item i , MakeModelDetails m , MakeModel mm where i.itemId = m.item.itemId && mm.make_model_id ")
+    @Query(value = "select i.itemName,mm.makeName,mm.modelName,i.quantityOnHand from Item i , MakeModelDetails m , MakeModel mm where i.itemId = m.item.itemId and mm.makeModelId= m.makeModel.makeModelId and i.quantityOnHand< m.stockLevel ")
     List<Object[]> getLowStockLevelReport();
 
 //    i.item_id = m. and mm.make_model_id= m.make_model_make_model_id and
